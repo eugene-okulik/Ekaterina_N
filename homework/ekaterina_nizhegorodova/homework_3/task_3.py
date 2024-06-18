6518245677284edd5c8a0c3e31e636cf8cac1a9b
PrincessOfAmber <interlighten@gmail.com>
2024-06-18 19:50:13 +0300

    homework_3 done

---
+++ homework/ekaterina_nizhegorodova/homework_3/task_3.py
@@ -0,0 +1,8 @@
+import math
+
+num_1 = 10
+num_2 = 20
+a_avg = (num_1 + num_2) / 2
+print("Среднее арифметическое = " + str(a_avg))
+g_avg = math.sqrt(num_1 * num_2)
+print("Среднее геометрическое = " + str(g_avg))
